64ed1e28923b7497dd79d7a4842cb80bb1958059
Ashwek Swamy <39827514+ashwek@users.noreply.github.com>
2018-06-01 16:54:54 +0530

    Update Prime_Number.py

--- Python_2/Prime_Number.py
+++ Python_2/Prime_Number.py
@@ -1,25 +1,24 @@
+from math import sqrt
+
 opt='y' # variable to store repeating condition
 
-while((opt=='y')|(opt=='Y')): # Loop to run program until asked to exit
-        
-	print "\n\nThis program is made by \"ASHWEK\" to check if a number is prime or not \n" # Intro
-	
-	try: # To handle Input Number Errors 
-		num = int(input("\t -> Enter a number to check if it is prime = ")) # To take input
-	
+while((opt=='y')|(opt=='Y')):
+
+	try:
+		num = int(raw_input("\n\tEnter a number to check if it is prime = "))	
 		if(num == 0):print "\n\t\"0\" is neither prime nor composite"
 		else:
-			if(num<0): num = -num # If input is negative make it positive
+			num = abs(num) # If input is negative make it positive
 
 			flag=0
-			for i in range(2, num): # Pinding if it is prime or not
+			for i in range(2, int(sqrt(num))+1):
 				if(num % i == 0):
 					flag = 1
 					break
 
-			if(flag == 0): print "\n\t", num, " is a PRIME number"
-			else: print "\n\t", num, " is NOT PRIME, it is divisible by ",i
+			if(flag == 0): print "\t->", num, "is a PRIME number"
+			else: print "\t->", num, "is NOT PRIME, it is divisible by ",i
 			
-	except NameError: print "\n*** Invalid Input ***"
+	except ValueError: print "\n\t*** Invalid Input ***"
 
-	opt=raw_input("\n\nDo you want to continue [Y/N] = ") # to ask user to continue or exit
+	opt=raw_input("\n\tDo you want to continue [Y/N] = ")
