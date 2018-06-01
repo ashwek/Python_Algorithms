d526d3ac151684577b9ed23b961a4a7a6d5e4f0b
Ashwek Swamy <39827514+ashwek@users.noreply.github.com>
2018-06-01 16:45:05 +0530

    Update Multiplication_Table.py

--- Python_2/Multiplication_Table.py
+++ Python_2/Multiplication_Table.py
@@ -1,27 +1,11 @@
-"""
+try:
+	num = float(raw_input("\n\tEnter a number = "))
+	try:
+		limit = int(raw_input("\tEnter a limit = "))
+		N_width = len(str(limit))
+		R_width = len(str(num*limit))
+		for n in range (1, limit+1):
+			print "\t ->", num, "*", str(n).center(N_width), "=", str(num*n).center(R_width)
 
-Multiplication Table
-
-"""
-
-opt='y' # Variable to store Repeating Condition
-
-while((opt=='y')|(opt=='Y')): # To run the program till asked to exit
-        
-        print "\n\nThis program is written by \"ASHWEK\" to print tables" # Intro
-        
-        try: # to handle Number Input error
-                num=float(input("\n\tEnter a number = ")) # To input a number
-                
-                try: # To handle limit Input error
-                        limit=int(input("\tEnter a limit = ")) # Limit Input
-                        
-                        print 
-
-                        for n in range (1,limit+1): # Loop to print the Tables
-                                print "\t -> ",num,"*",n,"=",(num*n)
-                                
-                        opt=raw_input("\nDo you want to continue [Y/N] = ") # To give chooice to continue or not
-                        
-                except NameError: print "*** Invalid Limit Entered ***"
-        except NameError: print "*** Invalid Number Entered ***"
+	except ValueError: print "\n\t*** Invalid Limit Entered ***\n"
+except ValueError: print "\n\t*** Invalid Number Entered ***\n"
