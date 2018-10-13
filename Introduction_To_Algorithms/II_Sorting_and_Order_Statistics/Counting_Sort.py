fc7e1aa7aa55cebdecc3d6ec5b219445c98a8b94
Ashwek Swamy <39827514+ashwek@users.noreply.github.com>
2018-10-13 23:36:45 +0530

    Create Counting_Sort.py

---
+++ Introduction_To_Algorithms/II_Sorting_and_Order_Statistics/Counting_Sort.py
@@ -0,0 +1,13 @@
+def Counting_Sort(A, k=-1):
+	if(k==-1): k=max(A)
+	C = [0 for x in range(k+1)]
+	for x in A: C[x]+=1
+	for x in range(1, k+1):C[x]+=C[x-1]
+	B = [0 for x in range(len(A))]
+	for x in A:
+		B[C[x]-1] = x
+		C[x]-=1
+	return B
+		
+A = [13,20,18,20,12,15,7]
+print(Counting_Sort(A))
