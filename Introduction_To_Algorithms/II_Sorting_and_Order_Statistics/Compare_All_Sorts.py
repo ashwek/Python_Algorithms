4b4b2a8b5425d58261e3b1bbe42d1dc115199c6f
Ashwek Swamy <39827514+ashwek@users.noreply.github.com>
2018-10-14 13:44:25 +0530

    Update Compare_All_Sorts.py

--- Introduction_To_Algorithms/II_Sorting_and_Order_Statistics/Compare_All_Sorts.py
+++ Introduction_To_Algorithms/II_Sorting_and_Order_Statistics/Compare_All_Sorts.py
@@ -81,6 +81,27 @@ Merge_Sort(A, 0, len(A))
 """, number=1)
 print("Merge Sort =".rjust(Print_Size), Merge, "seconds")
 
+Radix = timeit("""
+def Counting_Sort(A, d, Digit):
+	C = [0]*10
+	for x in A: C[int(str(x).rjust(Digit, '0')[d])]+=1
+	for x in range(1, 10): C[x]+=C[x-1]
+	B=list(range(len(A)))
+	for i in range(len(A)-1, -1, -1):
+		x = A[i]
+		B[C[int(str(x).rjust(Digit, '0')[d])]-1]=x
+		C[int(str(x).rjust(Digit, '0')[d])]-=1
+	return B
+
+def Radix_Sort(A, Digit):
+	for d in range(Digit-1, -1, -1):
+		A=Counting_Sort(A, d, Digit)
+	return A
+A = list(range(900,0,-1))
+A=Radix_Sort(A, 3)
+""", number=1)
+print("Radix Sort =".rjust(Print_Size), Radix, "seconds")
+
 Quick = timeit("""
 def Partition(A, low, high):
 	Pivot = A[high-1]
