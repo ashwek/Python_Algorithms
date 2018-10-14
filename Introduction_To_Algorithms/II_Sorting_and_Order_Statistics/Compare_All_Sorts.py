af8d6f38b783e01bf23612b00d00dab424e91993
Ashwek Swamy <39827514+ashwek@users.noreply.github.com>
2018-10-14 22:04:17 +0530

    Added Bucket_Sort

--- Introduction_To_Algorithms/II_Sorting_and_Order_Statistics/Compare_All_Sorts.py
+++ Introduction_To_Algorithms/II_Sorting_and_Order_Statistics/Compare_All_Sorts.py
@@ -21,6 +21,23 @@ Counting_Sort(A)
 """, number=1)
 print("Counting Sort =".rjust(Print_Size), Counting, "seconds")
 
+Bucket = timeit("""
+def Bucket_Sort(A, Digit):
+	B = [[] for x in range(10)]
+	for each in A: 
+		B[int(str(each).rjust(Digit, '0')[0])].append(each)
+	for each in range(10):B[each].sort()
+	A=[]
+	for bucket in B:
+		for each in bucket:
+			A.append(each)
+	return A
+	
+A = list(range(900,0,-1))
+A=Bucket_Sort(A, 3)
+""", number=1)
+print("Bucket Sort =".rjust(Print_Size), Bucket, "seconds")
+
 Heap = timeit("""
 from math import floor
 
