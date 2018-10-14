f85a5510daf68b9809aa7ff342f96f7030821378
Ashwek Swamy <39827514+ashwek@users.noreply.github.com>
2018-10-14 13:35:20 +0530

    Update Counting_Sort.py

--- Introduction_To_Algorithms/II_Sorting_and_Order_Statistics/Counting_Sort.py
+++ Introduction_To_Algorithms/II_Sorting_and_Order_Statistics/Counting_Sort.py
@@ -4,7 +4,8 @@ def Counting_Sort(A, k=-1):
 	for x in A: C[x]+=1
 	for x in range(1, k+1):C[x]+=C[x-1]
 	B = [0 for x in range(len(A))]
-	for x in A:
+	for i in range(len(A)-1, -1, -1):
+		x = A[i]
 		B[C[x]-1] = x
 		C[x]-=1
 	return B
