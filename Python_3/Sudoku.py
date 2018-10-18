05bbc5e912f7587f3319c17a36a38563a73660c4
Ashwek Swamy <39827514+ashwek@users.noreply.github.com>
2018-10-18 16:49:43 +0530

    Update Sudoku.py

--- Python_3/Sudoku.py
+++ Python_3/Sudoku.py
@@ -27,17 +27,12 @@ def Input(Sudoku):
 
 	for r in range(9):
 		for c in range(9):
-			if(Sudoku[r][c] == '-'):Sudoku[r][c]=0
-			else:Sudoku[r][c] = int(Sudoku[r][c])
-			
+			Sudoku[r][c] = [x for x in range(1,10)] if(Sudoku[r][c] == '-') else int(Sudoku[r][c])
+
 def In_Nonet(Sudoku, Value, R, C):
-	if(R>=0 and R<=2):R=0
-	elif(R>=3 and R<=5):R=3
-	else:R=6
-	
-	if(C>=0 and C<=2):C=0
-	elif(C>=3 and C<=5):C=3
-	else:C=6
+	Map = {0:0, 1:0, 2:0, 3:3, 4:3, 5:3, 6:6, 7:6, 8:6}
+	R = Map[R]
+	C = Map[C]
 	
 	for row in range(R, R+3):
 		for column in range(C, C+3):
@@ -45,7 +40,7 @@ def In_Nonet(Sudoku, Value, R, C):
 	return False
 	
 def In_Column(Sudoku, Value, Column):
-	for r in range(len(Sudoku)):
+	for r in range(9):
 		if(Value == Sudoku[r][Column]):return True
 	return False
 
@@ -58,29 +53,18 @@ def Remove_Repeating(Sudoku):
 				while(each<len(Sudoku[r][c])):
 					if(Sudoku[r][c][each] in Sudoku[r]):
 						Sudoku[r][c].remove(Sudoku[r][c][each])
-						each-=1
-					each+=1
+					else: each+=1
 				each=0
 				while(each<len(Sudoku[r][c])):
 					if(In_Column(Sudoku, Sudoku[r][c][each], c)): 
 						Sudoku[r][c].remove(Sudoku[r][c][each]);
-						each-=1
-					each+=1
+					else: each+=1
 				each=0
 				while(each<len(Sudoku[r][c])):
 					if(In_Nonet(Sudoku, Sudoku[r][c][each], r, c)):
 						Sudoku[r][c].remove(Sudoku[r][c][each]);
-						each-=1
-					each+=1
+					else: each+=1
 			except TypeError:pass
-			except:print(exc_info())
-			
-	
-def Add_List(Sudoku):
-	Lts=[1,2,3,4,5,6,7,8,9]
-	for r in range(9):
-		for c in range(9):
-			if(Sudoku[r][c]==0): Sudoku[r][c] = list(Lts)
 
 def Single(Sudoku):
 	for r in range(9):
@@ -90,23 +74,20 @@ def Single(Sudoku):
 			except:pass
 	
 def Solved(Sudoku):
-	Scr=0
+	Single_Blocks=0
 	for r in Sudoku:
 		for c in r:
 			try:len(c)
-			except:Scr+=1
-	if(Scr==81):return True
+			except:Single_Blocks+=1
+	if(Single_Blocks==81):return True
 	else:return False
 						
 def Solve(Sudoku):
-	Add_List(Sudoku)
-	Remove_Repeating(Sudoku)
 	while(not Solved(Sudoku)):
-		Single(Sudoku)	
 		Remove_Repeating(Sudoku)
-			
-system("clear")
+		Single(Sudoku)
 
+system("clear")
 Sudoku=[]
 print("\n\tEnter Sudoku Below (use - for blank Spots): ")
 Input(Sudoku)
