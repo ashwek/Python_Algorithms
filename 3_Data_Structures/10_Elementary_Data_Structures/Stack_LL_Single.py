3bcf2d2b7f93fd0c240e5871dddc68156cb4e8bb
ashwek <ashwekswamy@protonmail.com>
2018-12-03 00:02:46 +0530

    Update Stack LL

--- 3_Data_Structures/10_Elementary_Data_Structures/Stack_LL_Single.py
+++ 3_Data_Structures/10_Elementary_Data_Structures/Stack_LL_Single.py
@@ -1,63 +1,62 @@
-class Node:
-	def __init__(self, Value, Next=None):
-		self.__value = Value
-		self.__next = Next
+class Node:         # Class to create Nodes
+    def __init__(self, Value, Next=None):
+        self.value = Value      # value in Node
+        self.next = Next        # points to next node
 
-	def get_value(self): return self.__value
-	def get_next(self): return self.__next
-	
-class LL_Single:
-	def __init__(self):
-		self._top = None
-		self._len = 0
-		
-	def isEmpty(self): return self._top==None
-	
-	def Push(self, data):
-		self._top = Node(data, self._top)
-		self._len+=1
-	
-	def Pop(self):
-		if(self.isEmpty()): raise IndexError("Stack is Empty")
-		else:
-			temp = self._top
-			self._top = self._top.get_next()
-			self._len-=1
-			return temp.get_value()
-			
-	def __len__(self): return self._len
-	
-	def Elements(self):
-		if(self.isEmpty()): raise StopIteration("Stack is Empty")
-		else:
-			temp = self._top
-			while(temp != None): 
-				yield temp.get_value()
-				temp=temp.get_next()
-			
-def Push(S_LL1): S_LL1.Push(input("\n\tEnter a value = "))
+class Stack_LL:
+    def __init__(self):
+        self.__top = None       # initially top = None
+        self.__len = 0          # initially length of stack = 0
 
-def Pop(S_LL1):
-	if(S_LL1.isEmpty()): print("\n\tStack Empty")
-	else: print("\n\tPopped value = ", S_LL1.Pop())
+    def is_empty(self):
+        return self.__top == None
 
-def Display(S_LL1):
-	print("\n\tStack : (", end='')
-	for each in S_LL1.Elements(): print(each, end=', ')
-	print(')')
+    def push(self, value):
+        self.__top = Node(value, self.__top)        # Create Node with value = "value" & next = "top". Make new node as top
+        self.__len += 1         # increment length
+
+    def pop(self):
+        if(self.is_empty()): raise IndexError("Stack is Empty")         # check if stack is empty
+        else:
+            temp = self.__top               # temp = top
+            self.__top = self.__top.next    # top = next element
+            temp.next = None                # (previous top) temp.next -> None (break the link)
+            self.__len -= 1                 # decrement length
+            return temp.value               # return poped value
+
+    def __len__(self):
+        return self.__len
+
+    def __str__(self):          # return str form of linked list i.e., val1 -> val2 -> None
+        temp = self.__top
+        Values = []
+        while temp:
+            Values.append(str(temp.value))      # add values to "Values"
+            temp = temp.next
+        Values.append("None")
+        return " -> ".join(Values)          # return all Values as string
+
+    # __next__ and __iter__ methods are used to iterate through the object
+
+    # __next__ is used to get the next element in the iterable object
+    def __next__(self):
+        temp = self.__i
+        if (temp == None): raise StopIteration() # if temp = None, temp has reached the end, raise StopIteration to stop iteration
+        self.__i = self.__i.next
+        return temp.value
+
+    # __iter__ returns an iterable object (self) & sets i = top, to iterate through top to end
+    def __iter__(self):
+        self.__i = self.__top
+        return self
 
 if __name__ == '__main__':
-	S_LL1 = LL_Single()
-	while 1:
-		print("\n\t1. Push\n\t2. Pop\n\t3. Display Stack\n\t4. Length of Stack\n\t5. Exit")
-		try: ch = int(input("\tEnter your choice = "))
-		except ValueError: ch=0
-		
-		if(ch==1): Push(S_LL1)
-		elif(ch==2): Pop(S_LL1)
-		elif(ch==3): Display(S_LL1)
-		elif(ch==4): print("\n\tLength = ", len(S_LL1))
-		elif(ch==5): break
-		else: print("\n\tInvalid Input ")
-		input("\n\tPress Enter to continue...")
-		
+    S1 = Stack_LL()
+
+    for i in range(5, 21, 5):
+        print("push", i, "to stack")
+        S1.push(i)
+
+    print("\nStack : ", S1)
+
+    print("\nPop stack, popped value = ", S1.pop())
