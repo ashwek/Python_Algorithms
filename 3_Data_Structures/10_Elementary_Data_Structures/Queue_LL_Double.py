de8d6da986b43f83f1a7726c7b04d68135855eea
ashwek <ashwekswamy@protonmail.com>
2018-12-03 18:24:49 +0530

    Update Queue_LL_Double.py

--- 3_Data_Structures/10_Elementary_Data_Structures/Queue_LL_Double.py
+++ 3_Data_Structures/10_Elementary_Data_Structures/Queue_LL_Double.py
@@ -1,71 +1,69 @@
 class Node:
-	def __init__(self, Value=None, Prev=None, Next=None):
-		self.__value = Value
-		self.__next = Next
-		self.__prev = Prev
 
-	def get_value(self): return self.__value
-	def set_next(self, Next): self.__next=Next
-	def get_next(self): return self.__next
-	def set_prev(self, Prev): self.__prev=Prev
-	def get_prev(self): return self.__prev
-			
-class LL_Double:
-	def __init__(self):
-		self._head = Node()
-		self._tail = Node(Prev=self._head)
-		self._head.set_next(self._tail)
-		self._len = 0
-		
-	def isEmpty(self): return self._len==0
-	
-	def EnQueue(self, Value):
-		temp = Node(Value, self._tail.get_prev(), self._tail)
-		self._tail.get_prev().set_next(temp)
-		self._tail.set_prev(temp)
-		self._len+=1
-	
-	def DeQueue(self):
-		if(self.isEmpty()): raise IndexError("Queue is Empty")
-		else:
-			temp = self._head.get_next()
-			self._head.set_next(temp.get_next())
-			temp.get_next().set_prev(self._head)
-			self._len-=1
-			return temp.get_value()
-			
-	def __len__(self): return self._len
-	
-	def Elements(self):
-		if(self.isEmpty()): raise StopIteration("Queue is Empty")
-		else:
-			temp = self._head.get_next()
-			while(temp != self._tail): 
-				yield temp.get_value()
-				temp=temp.get_next()
-		
-def EnQueue(Q_LL1): Q_LL1.EnQueue(input("\n\tEnter a value = "))
+    def __init__(self, Value=None, Prev=None, Next=None):
+        self.value = Value
+        self.next = Next
+        self.prev = Prev
 
-def DeQueue(Q_LL1):
-	if(Q_LL1.isEmpty()): print("\n\tQueue is Empty")
-	else: print("\n\tDeleted value = ", Q_LL1.DeQueue())
+class Queue_LLD:
 
-def Display(Q_LL1):
-	print("\n\tQueue : (", end='')
-	for each in Q_LL1.Elements(): print(each, end=', ')
-	print(')')
+    def __init__(self):
+        self.__head = Node()
+        self.__tail = Node(Prev = self.__head)
+        self.__head.next = self.__tail
+        self.__len = 0
+        self.__i = None
+
+    def is_empty(self):
+        return self.__len == 0
+
+    def enqueue(self, Value):
+        temp = Node(Value, self.__tail.prev, self.__tail)
+        self.__tail.prev.next  = temp
+        self.__tail.prev = temp
+        self.__len += 1
+
+    def dequeue(self):
+        if(self.is_empty()): raise IndexError("Queue is Empty")
+        else:
+            temp = self.__head.next
+            self.__head.next = temp.next
+            temp.next.prev = self.__head
+            self.__len -= 1
+            return temp.value
+
+    def __len__(self):
+        return self.__len
+
+    def __next__(self):
+        if( self.__i != self.__tail ):
+            self.__i = self.__i.next
+            return self.__i.prev.value
+        else: raise StopIteration()
+
+    def __iter__(self):
+        self.__i = self.__head.next
+        return self
+
+    def __str__(self):
+        Values = []
+        for i in self:
+            Values.append(str(i))
+
+        return ", ".join(Values)
 
 if __name__ == '__main__':
-	Q_LL1 = LL_Double()
-	while 1:
-		print("\n\t1. EnQueue\n\t2. DeQueue\n\t3. Display Queue\n\t4. Length of Queue\n\t5. Exit")
-		try: ch = int(input("\tEnter your choice = "))
-		except ValueError: ch=0
-		
-		if(ch==1): EnQueue(Q_LL1)
-		elif(ch==2): DeQueue(Q_LL1)
-		elif(ch==3): Display(Q_LL1)
-		elif(ch==4): print("\n\tLength = ", len(Q_LL1))
-		elif(ch==5): break
-		else: print("\n\tInvalid Input ")
-		input("\n\tPress Enter to continue...")
+
+    Q_LL1 = Queue_LLD()
+
+    for i in range(5):
+        print("Insert", i)
+        Q_LL1.enqueue(i)
+
+    print("\nQueue :", end="")
+    for i in Q_LL1:
+        print(i, end=", ")
+
+    print("\n\nDequeue, Deleted value = ", Q_LL1.dequeue())
+
+    print("\nDequeue = ", Q_LL1)
