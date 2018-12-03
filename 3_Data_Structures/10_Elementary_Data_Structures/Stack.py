0dc2149ceb3d2c2ca337605717fcc0b9bed964a4
ashwek <ashwekswamy@protonmail.com>
2018-12-03 17:40:54 +0530

    Update Stack.py

--- 3_Data_Structures/10_Elementary_Data_Structures/Stack.py
+++ 3_Data_Structures/10_Elementary_Data_Structures/Stack.py
@@ -1,69 +1,63 @@
 class Stack:
-	
-	def __init__(self, Size=10):
-		self._size = Size
-		self._stack=[]
-		self._i=-1
-	
-	def isEmpty(self): return len(self._stack) == 0
-	def isFull(self): return len(self._stack) == self._size
-	
-	def Push(self, value):
-		if(self.isFull()): raise IndexError("Overflow Error - Stack is Full")
-		else: self._stack.append(value)
-	
-	def Pop(self):
-		if(self.isEmpty()): raise IndexError("Unerflow Error - Stack is Empty")
-		return self._stack.pop()
-		
-	def Top(self):
-		if(self.isEmpty()): raise IndexError("Stack is Empty")
-		return self._stack[-1]
-	
-	def __len__(self):return len(self._stack)
-		
-	def __getitem__(self, index): return self._stack[index]
-		
-	def __next__(self):
-		self._i+=1 
-		if(self._i < self.__len__()): return self.__getitem__(self._i)
-		else: raise StopIteration()
-		
-	def __iter__(self):
-		self._i=-1
-		return self
-		
-def Push(S1):
-	if(S1.isFull()): print("\n\tOverflow Error - Stack is Full")
-	else: S1.Push(input("\n\tEnter value to push in stack = "))
-	
-def Pop(S1):
-	try: print("\n\tPopped Value = ", S1.Pop())
-	except IndexError: print("\n\tUnerflow Error - Stack is Empty")
-	
-def See_Top(S1):
-	try: print("\n\tTop Value = ", S1.Top())
-	except IndexError: print("\n\tStack is Empty")
-	
-def Display(S1):
-	print("\n\tStack : ", end='')
-	for each in S1: print(each, end=', ')
-		
+
+    def __init__(self, Size=10):
+        self.__size = Size
+        self.__stack = []
+        self.__i = -1
+
+    def is_empty(self):
+        return len(self.__stack) == 0
+
+    def is_full(self):
+        return len(self.__stack) == self.__size
+
+    def push(self, value):
+        if(self.is_full()): raise IndexError("Overflow Error - Stack is Full")
+        else: self.__stack.append(value)
+
+    def pop(self):
+        if(self.is_empty()): raise IndexError("Unerflow Error - Stack is Empty")
+        return self.__stack.pop()
+
+    def top(self):
+        if(self.is_empty()): raise IndexError("Stack is Empty")
+        return self.__stack[-1]
+
+    def __len__(self):
+        return len(self.__stack)
+
+    def __getitem__(self, index):
+        if(index < len(self)): return self.__stack[index]
+        else: raise IndexError()
+
+    def __next__(self):
+        self.__i += 1
+        if(self.__i < len(self)): return self.__stack[self.__i]
+        else: raise StopIteration()
+
+    def __iter__(self):
+        self.__i = -1
+        return self
+
+    def __str__(self):
+        Values = []
+        for i in self.__stack:
+            Values.append(str(i))
+
+        return ", ".join(Values)
+
 if __name__ == '__main__':
-	try: Size = int(input("\n\tEnter size of stack = "))
-	except ValueError: Size=5
-	
-	S1 = Stack(Size)
-	while 1:
-		print("\n\t1. Push\n\t2. Pop\n\t3. See Top\n\t4. Display Stack\n\t5. Length of Stack\n\t6. Exit")
-		try: ch = int(input("\tEnter your choice = "))
-		except ValueError: ch=0
-		
-		if(ch==1): Push(S1)
-		elif(ch==2): Pop(S1)
-		elif(ch==3): See_Top(S1)
-		elif(ch==4): Display(S1)
-		elif(ch==5): print("\n\tLength = ", len(S1))
-		elif(ch==6): break
-		else: print("\n\tInvalid Input ")
-		input("\n\tPress Enter to continue...")
+
+    S1 = Stack(5)
+
+    for i in range(5):
+        print("Insert", i, "in stack")
+        S1.push(i)
+
+    print("\nStack : ", end="")
+    for i in S1:
+        print(i, end=", ")
+
+    print("\n\nPop, popped element = ", S1.pop())
+
+    print("\nStack :", S1)
