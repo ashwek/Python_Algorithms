35eb66b21cbaa598512daf962911f333236463c2
ashwek <ashwekswamy@protonmail.com>
2018-12-03 17:51:34 +0530

    Update Queue_Circular.py

--- 3_Data_Structures/10_Elementary_Data_Structures/Queue_Circular.py
+++ 3_Data_Structures/10_Elementary_Data_Structures/Queue_Circular.py
@@ -1,84 +1,72 @@
 class Queue_Circular:
-	
-	def __init__(self, Size=10):
-		self._size = Size+1
-		self._queue=[None]*(Size+1)
-		self._head = self._tail = 0
-		self._i=-1
-	
-	def isEmpty(self): return self._head == self._tail
-	def isFull(self): return (self._tail+1)%self._size == self._head
-	
-	def EnQueue(self, value):
-		if(self.isFull()): raise IndexError("Overflow Error - Queue is Full")
-		self._queue[self._tail]=value
-		self._tail = (self._tail+1)%self._size
-		
-	def DeQueue(self):
-		if(self.isEmpty()): raise IndexError("Unerflow Error - Queue is Empty")
-		self._head = (self._head+1)%self._size
-		return self._queue[(self._head-1)%self._size]
-		
-	def Head(self):
-		if(self.isEmpty()): raise IndexError("Queue is Empty")
-		return self._queue[self._head]
-	
-	def Tail(self):
-		if(self.isEmpty()): raise IndexError("Queue is Empty")
-		return self._queue[(self._tail-1)%self._size]
-	
-	def __len__(self):
-		return self._tail-self._head if(self._tail>=self._head) else self._size-self._head+self._tail
-		
-	def __getitem__(self, index):
-		if(0 <= index < self.__len__()):
-			return self._queue[(index+self._head)%self._size]
-		else: raise IndexError("Index out of range")
-		
-	def __next__(self):
-		self._i+=1 
-		if(self._i < self.__len__()): return self.__getitem__(self._i)
-		else: raise StopIteration()
-		
-	def __iter__(self):
-		self._i=-1
-		return self
-	
-	def Status(self): print("\n\tHead = ", self._head, "  Tail = ", self._tail)
 
-def Enqueue(Q1):
-	if(Q1.isFull()): print("\n\tOverflow Error - Queue is Full")
-	else: Q1.EnQueue(input("\n\tEnter value to enqueue in Queue = "))
-	
-def Dequeue(Q1):
-	try: print("\n\tDequeued Value = ", Q1.DeQueue())
-	except IndexError: print("\n\tUnerflow Error - Queue is Empty")
-	
-def See_Head_Tail(Q1):
-	try: print("\n\tHead Value = ", Q1.Head(), "\tTail Value = ", Q1.Tail())
-	except IndexError: print("\n\tQueue is Empty")
-	
-def Display(Q1):
-	print("\n\tQueue : ", end='')
-	for each in Q1: print(each, end=', ')
-		
+    def __init__(self, Size=10):
+        self.__size = Size+1
+        self.__queue = [None]*(Size+1)
+        self.__head = self.__tail = 0
+        self.__i = -1
+
+    def is_empty(self):
+        return self.__head == self.__tail
+
+    def is_full(self):
+        return (self.__tail+1)%self.__size == self.__head
+
+    def enqueue(self, value):
+        if(self.is_full()): raise IndexError("Overflow Error - Queue is Full")
+        self.__queue[self.__tail] = value
+        self.__tail = (self.__tail+1)%self.__size
+
+    def dequeue(self):
+        if(self.is_empty()): raise IndexError("Unerflow Error - Queue is Empty")
+        self.__head = (self.__head+1)%self.__size
+        return self.__queue[(self.__head-1)%self.__size]
+
+    def head(self):
+        if(self.is_empty()): raise IndexError("Queue is Empty")
+        return self.__queue[self.__head]
+
+    def tail(self):
+        if(self.is_empty()): raise IndexError("Queue is Empty")
+        return self.__queue[(self.__tail-1)%self.__size]
+
+    def __len__(self):
+        return self.__tail-self.__head if(self.__tail>=self.__head) else self.__size-self.__head+self.__tail
+
+    def __getitem__(self, index):
+        if(0 <= index < len(self)):
+            return self.__queue[(index+self.__head)%self.__size]
+        else: raise IndexError("Index out of range")
+
+    def __next__(self):
+        self.__i += 1
+        if(self.__i < len(self)): return self.__getitem__(self.__i)
+        else: raise StopIteration()
+
+    def __iter__(self):
+        self.__i = -1
+        return self
+
+    def __str__(self):
+        Values = []
+        for i in self:
+            Values.append(str(i))
+
+        return ", ".join(Values)
+
+
 if __name__ == '__main__':
-	try: Size = int(input("\n\tEnter size of queue = "))
-	except ValueError: Size=5
-	
-	Q1 = Queue_Circular(Size)
-	while 1:
-		Q1.Status()
-		print("\n\t1. Enqueue\n\t2. Dequeue\n\t3. See Head and Tail\n\t4. Display Queue\n\t5. Length of Queue\n\t6. Exit")
-		try: ch = int(input("\tEnter your choice = "))
-		except ValueError: ch=0
-		
-		if(ch==1): Enqueue(Q1)
-		elif(ch==2): Dequeue(Q1)
-		elif(ch==3): See_Head_Tail(Q1)
-		elif(ch==4): Display(Q1)
-		elif(ch==5): print("\n\tLength = ", len(Q1))
-		elif(ch==6): break
-		else: print("\n\tInvalid Input ")
-		input("\n\tPress Enter to continue...")
-		
+
+    Q1 = Queue_Circular(5)
+
+    for i in range(5):
+        print("Insert / enqueue", i)
+        Q1.enqueue(i)
+
+    print("\nQueue : ", end="")
+    for i in Q1:
+        print(i, end=", ")
+
+    print("\n\nDelete / dequeue, deleted value = ", Q1.dequeue())
+
+    print("\nQueue :", Q1)
